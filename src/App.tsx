3efc0932bd4980c099574d1a859caae50de7d380
Shamsher <shamsher@geekyants.com>
2022-02-16 21:34:28 +0530

    graph fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import { Outlet} from 'react-router-dom';
 import Header from './components/Header';
 import NavBarTop from './components/NavBarTop';
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
