2011b14e5301635515bc2ea3e2a8e7c31278aff5
Shamsher <shamsher@geekyants.com>
2022-02-17 12:50:23 +0530

    build

--- src/components/cards/WorkingHoursChart.js
+++ src/components/cards/WorkingHoursChart.js
@@ -1,81 +1,79 @@
-// import { Bar } from 'react-chartjs-2';
-// import Chart from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
 
-// function WorkingHoursChart() {
-//     const chart = new Chart;
-//     console.log(chart);
-//     const state = {
-//         labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-//         datasets: [
-//             {
-//                 label: 'Cat1',
-//                 backgroundColor: 'rgba(107, 137, 230,1)',
-//                 borderWidth: 1,
-//                 borderRadius: Number.MAX_VALUE,
-//                 borderSkipped: true,
-//                 data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
-//             },
-//             {
-//                 label: 'Cat2',
-//                 backgroundColor: 'rgba(100, 179, 235,1)',
-//                 borderWidth: 1,
-//                 borderRadius: Number.MAX_VALUE,
-//                 borderSkipped: true,
-//                     data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
-//             },
-//             {
-//                 label: 'Cat3',
-//                 backgroundColor: 'rgba(226, 226, 235,1)',
-//                 borderWidth: 1,
-//                 borderRadius: Number.MAX_VALUE,
-//                 borderSkipped: true,
-//                     data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
-//             }
-//         ]
-//     }
+function WorkingHoursChart() {
+    const state = {
+        labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+        datasets: [
+            {
+                label: 'Cat1',
+                backgroundColor: 'rgba(107, 137, 230,1)',
+                borderWidth: 1,
+                borderRadius: Number.MAX_VALUE,
+                borderSkipped: true,
+                data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
+            },
+            {
+                label: 'Cat2',
+                backgroundColor: 'rgba(100, 179, 235,1)',
+                borderWidth: 1,
+                borderRadius: Number.MAX_VALUE,
+                borderSkipped: true,
+                    data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
+            },
+            {
+                label: 'Cat3',
+                backgroundColor: 'rgba(226, 226, 235,1)',
+                borderWidth: 1,
+                borderRadius: Number.MAX_VALUE,
+                borderSkipped: true,
+                    data: [4, 2, 1, 3, 4, 3, 2, 1, 3, 4, 1, 2],
+            }
+        ]
+    }
 
-//     return (
-//         <div className='flex-auto h-64 p-4'>
+    return (
+        <div className='flex-auto h-64 p-4'>
 
-//             <Bar
+            <Bar
             
-//                 data={state}
-//                 legend={
-//                     {
-//                         display: false,
-//                     }
-//                 }
-//                 options={
-//                     {
-//                         legend: {
-//                             display: false,
-//                             position: 'bottom'
-//                         },
-//                         responsive: true,
-//                         barThickness: 12,
-//                         borderRadius: 3,
-//                         scales: {
-//                             x: {
-//                                 stacked: true,
-//                             },
-//                             y: {
-//                                 stacked: true
-//                             }
-//                         },
+                data={state}
+                legend={
+                    {
+                        display: false,
+                    }
+                }
+                options={
+                    {
+                        legend: {
+                            display: false,
+                            position: 'bottom'
+                        },
+                        responsive: true,
+                        barThickness: 12,
+                        borderRadius: 3,
+                        scales: {
+                            x: {
+                                stacked: true,
+                            },
+                            y: {
+                                stacked: true
+                            }
+                        },
                       
-//                         maintainAspectRatio: false,
-//                         tooltips: {
-//                             callbacks: {
-//                                 label: function (tooltipItem) {
-//                                     return tooltipItem.yLabel;
-//                                 }
-//                             }
-//                         },
-//                     }
-//                 }
-//             />
+                        maintainAspectRatio: false,
+                        tooltips: {
+                            callbacks: {
+                                label: function (tooltipItem) {
+                                    return tooltipItem.yLabel;
+                                }
+                            }
+                        },
+                    }
+                }
+            />
           
-//         </div>
-//     );
-// }
-// export default WorkingHoursChart;
+        </div>
+    );
+}
+export default WorkingHoursChart;
