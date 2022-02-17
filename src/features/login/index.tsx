6cd23e80ba0a9f49086125101fe4231bb06f1132
Shamsher <shamsher@geekyants.com>
2022-02-17 22:00:37 +0530

    responsive login

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -1,16 +1,23 @@
 import {ReactComponent as UserOnDesk} from './../../styles/assets/images/user-on-desk.svg';
 import LoginButton from '../../components/LoginButton';
+import Logo from './../../styles/assets/images/geekyants-logo.png';
 
 import Text from '../../components/Text';
 
 const UserLogin = () => {
     return (
         <>
-            <div className="flex login-gradient h-screen place-items-center">
-                <UserOnDesk className='flex-auto sticky p-6'/>
-                <div className="flex-auto">
-                    <Text text="Log In to Your Account" />
-                    <LoginButton />
+            <div className="sm:flex sm:flex-row login-gradient h-screen">
+                <div className='flex flex-col basis-2/3 space-y-4 '>
+                    <UserOnDesk className='flex-auto p-6  w-96 sm:w-full self-center'/>
+                </div>
+                
+                <div className="flex flex-col basis-1/4 self-center">
+                    <div className='flex-auto self-center'>
+                    <img src={Logo} alt="Geekyants"/>
+                        <Text text="Log In to Your Account" />
+                        <LoginButton />
+                    </div>
                 </div>
             </div>
         </>
