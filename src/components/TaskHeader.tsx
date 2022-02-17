510221a2a0336a529c452e18b8ccff5fd057f735
Shamsher <shamsher@geekyants.com>
2022-02-17 12:39:26 +0530

    build

--- src/components/TaskHeader.tsx
+++ src/components/TaskHeader.tsx
@@ -11,9 +11,9 @@ const TaskHeader = ({filterHandler,filter}:{filterHandler:any,filter:string}) =>
             </div>
             <div className="flex-none flex filter-border">
             
-                <button className={(filter == 'all') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('all')}>All</button>
-                <button className={(filter == 'pending') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('pending')}>Pending</button>
-                <button className={(filter == 'completed') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('completed')}>Completed</button>
+                <button className={(filter === 'all') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('all')}>All</button>
+                <button className={(filter === 'pending') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('pending')}>Pending</button>
+                <button className={(filter === 'completed') ? `${className} flex-auto p-1 pr-4 pl-4` : `flex-auto p-1 pr-4 pl-4`} onClick={() => filterHandler('completed')}>Completed</button>
             </div>
             <div className="flex-none p-2 ml-4 filter-border">
                 <Filter className="w-18 h-18" />
